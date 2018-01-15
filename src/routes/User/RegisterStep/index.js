468e46c02b6151dc40ac20407a6ba3556cca1b59
aisic <wuzehao16@qq.com>
2018-01-15 11:32:09 +0800

    register roadhog-api-doc

---
+++ src/routes/User/RegisterStep/index.js
@@ -0,0 +1,53 @@
+import React, { PureComponent } from 'react';
+import { Route, Redirect, Switch } from 'dva/router';
+import { Card, Steps } from 'antd';
+import PageHeaderLayout from '../../../layouts/PageHeaderLayout';
+import NotFound from '../../Exception/404';
+import { getRoutes } from '../../../utils/utils';
+import styles from '../style.less';
+
+const { Step } = Steps;
+
+export default class StepForm extends PureComponent {
+  getCurrentStep() {
+    const { location } = this.props;
+    const { pathname } = location;
+    const pathList = pathname.split('/');
+    switch (pathList[pathList.length - 1]) {
+      case 'info': return 0;
+      case 'confirm': return 1;
+      case 'result': return 2;
+      default: return 0;
+    }
+  }
+  render() {
+    const { match, routerData } = this.props;
+    return (
+      <div>
+        <Card bordered={false}>
+          <div>
+            <Steps current={this.getCurrentStep()} className={styles.steps}>
+              <Step title="填写转账信息" />
+              <Step title="确认转账信息" />
+              <Step title="完成" />
+            </Steps>
+            <Switch>
+              {
+                getRoutes(match.path, routerData).map(item => (
+                  <Route
+                    key={item.key}
+                    path={item.path}
+                    component={item.component}
+                    exact={item.exact}
+                  />
+                ))
+              }
+              <Redirect exact from="/user/register" to="/user/register/info" />
+              <Route render={NotFound} />
+            </Switch>
+          </div>
+        </Card>
+      </div>
+    );
+  }
+}
