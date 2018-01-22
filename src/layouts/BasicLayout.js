6592e3cea50d899f32657fe6d44f97081128c36f
aisic <wuzehao16@qq.com>
2018-01-22 15:41:00 +0800

    change title to 众银云测

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -93,9 +93,9 @@ class BasicLayout extends React.PureComponent {
   getPageTitle() {
     const { routerData, location } = this.props;
     const { pathname } = location;
-    let title = 'Ant Design Pro';
+    let title = '众银云测';
     if (routerData[pathname] && routerData[pathname].name) {
-      title = `${routerData[pathname].name} - Ant Design Pro`;
+      title = `${routerData[pathname].name} - 众银云测`;
     }
     return title;
   }
