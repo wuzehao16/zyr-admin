705d27e8e2af7fd64fa319625e45952547ed5f4d
aisic <wuzehao16@qq.com>
2018-02-07 17:46:08 +0800

    order > review

--- src/routes/Order/Review.js
+++ src/routes/Order/Review.js
@@ -3,14 +3,16 @@ import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
 import moment from 'moment';
 import {
-  Form, Button, Card, Divider, Steps,
+  Form, Button, Card, Divider, Steps, Col , Select, Input
 } from 'antd';
 import DescriptionList from '../../components/DescriptionList';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
 const { Description } = DescriptionList;
+const FormItem = Form.Item;
 const Step = Steps.Step;
-
+const { TextArea } = Input;
+const Option = Select.Option;
 @connect(({ order }) => ({
   data: order,
 }))
@@ -20,6 +22,17 @@ export default class BasicForms extends PureComponent {
     const { setFieldsValue } = this.props.form;
     setFieldsValue({
       sysMenus: value,
+    });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        this.props.dispatch({
+          type: 'order/updateOrderState',
+          payload: values,
+        });
+      }
     });
   }
   renderForm() {
@@ -40,7 +53,9 @@ export default class BasicForms extends PureComponent {
   }
   renderDefault = ()=> {
     const { submitting, data: { item }, dispatch } = this.props
+    const { getFieldDecorator, getFieldValue } = this.props.form;
     return (
+      <div>
       <Steps progressDot  current={item.orderStauts} style={{ marginBottom: 80 }}>
         <Step title="申请中" />
         <Step title="已申请" />
@@ -49,6 +64,21 @@ export default class BasicForms extends PureComponent {
         <Step title="已面签" />
         <Step title="已放款" />
       </Steps>
+      <DescriptionList size="large" title="" style={{ marginBottom: 32 }} col={2}>
+      <Description term="更新状态">
+        <Col sm={12} xs={24}>
+          <FormItem >
+            {getFieldDecorator('orderStatus')(
+              <Select placeholder="请选择" style={{ width: '100%' }}>
+                <Option value={item.orderStauts}>下一步</Option>
+                <Option value={6}>拒绝</Option>
+              </Select>
+          )}
+          </FormItem>
+        </Col>
+      </Description>
+      </DescriptionList>
+      </div>
     )
   }
   renderReject = ()=> {
@@ -97,10 +127,17 @@ export default class BasicForms extends PureComponent {
     )
   }
   render() {
-    const { submitting, data: { item }, dispatch } = this.props
+    const { submitting, data: { item }, dispatch } = this.props;
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+
     return (
       <PageHeaderLayout title="订单详情" >
         <Card bordered={false}>
+          <Form
+            onSubmit={this.handleSubmit}
+            hideRequiredMark
+            style={{ marginTop: 8 }}
+          >
           <DescriptionList size="large" title="基本信息" style={{ marginBottom: 32 }} col={2}>
             <Description term="订单号">{item.orderNo}</Description>
             <Description term="更新时间">{item.updateTime}</Description>
@@ -128,11 +165,31 @@ export default class BasicForms extends PureComponent {
             <Description term="申请备注">{item.applicationNotes}</Description>
           </DescriptionList>
           {this.renderForm()}
+          <DescriptionList col={1} style={{ marginBottom: 32 }}>
+          <Description
+            term="拒绝原因"
+            style={{
+            display: getFieldValue('orderStatus') === 6 ? 'block' : 'none',
+          }}
+          >
+            <Col sm={12} xs={24}>
+              <FormItem >
+                {getFieldDecorator('cancelReason')(
+                  <TextArea  rows={4}/>
+              )}
+              </FormItem>
+            </Col>
+          </Description>
+          </DescriptionList>
           <DescriptionList size="large" style={{ marginBottom: 32, textAlign: 'center' }} col={1}>
+            <Button style={{ marginRight: 50 }} type="primary" htmlType="submit" loading={submitting}>
+              保存
+            </Button>
             <Button style={{ marginRight: 50 }} onClick={() => dispatch(routerRedux.push('/order'))}>
             返回
             </Button>
           </DescriptionList>
+          </Form>
         </Card>
       </PageHeaderLayout>
     );
