c76c7a9d3f5755d4bcabeab8f050eb3659998c21
aisi <wuzehao16@qq.com>
2018-03-08 14:17:45 +0800

    change e2e test

--- src/e2e/login.e2e.js
+++ src/e2e/login.e2e.js
@@ -17,19 +17,19 @@ describe('Login', () => {
   afterEach(() => page.close());
 
   it('should login with failure', async () => {
-    await page.type('#userName', 'mockuser');
-    await page.type('#password', 'wrong_password');
+    await page.type('#loginAccount', 'mockuser');
+    await page.type('#loginPassord', 'wrong_password');
     await page.click('button[type="submit"]');
     await page.waitForSelector('.ant-alert-error'); // should display error
   });
 
   it('should login successfully', async () => {
-    await page.type('#userName', 'admin');
-    await page.type('#password', '888888');
+    await page.type('#loginAccount', '小宇宙');
+    await page.type('#loginPassord', '123456');
     await page.click('button[type="submit"]');
     await page.waitForSelector('.ant-layout-sider h1'); // should display error
     const text = await page.evaluate(() => document.body.innerHTML);
-    expect(text).toContain('<h1>Ant Design Pro</h1>');
+    expect(text).toContain('<h1>众银云测</h1>');
   });
 
   afterAll(() => browser.close());
