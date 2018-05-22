3694bbfd7846773f03d59911f01cbef4912e80df
aisi <wuzehao16@qq.com>
2018-05-22 14:28:35 +0800

    change token

--- src/rollbar.js
+++ src/rollbar.js
@@ -3,7 +3,7 @@ import Rollbar from 'rollbar';
 // Track error by rollbar.com
 if (location.host === 'app.ibankmatch.com') {
   Rollbar.init({
-    accessToken: 'b1bae8b02bd8478baff2b26f691580ee',
+    accessToken: '563c06e855f44dc89a14a321df0567be',
     captureUncaught: true,
     captureUnhandledRejections: true,
     payload: {
