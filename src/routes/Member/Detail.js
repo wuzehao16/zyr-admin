3fb48b4d55c3b0dadf340a90ad462daa75c474b4
LingWuya <478399567@qq.com>
2018-06-07 16:06:41 +0800

    '公司资质审核接口调试'

--- src/routes/Member/Detail.js
+++ src/routes/Member/Detail.js
@@ -106,7 +106,7 @@ export default class BasicForms extends PureComponent {
                 </Description>
                 :<div></div>
               }
-              <Description term="招聘资格">{item.appCompany.status === 1 ? '有' : '无'}</Description>
+              <Description term="招聘资格">{item.appCompany.status == 1 ? '有' : '无'}</Description>
             </DescriptionList>
             <Divider style={{ marginBottom: 32, width:'70%', marginLeft:'15%'  }} />
           </div>:null}
